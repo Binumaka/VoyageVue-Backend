1f088b484c7ac6792b8b66c4c1d3faf4fa4d5a2f
Binu Maka <binumaka9@gmail.com>
2024-12-25 21:27:27 +0545

    adding routes

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express= require("express")
 const connectDB= require("./config/db")
 const app= express();
-const userRoute= require("./routes/userRoutes")
+const userRoutes= require("./routes/userRoutes");
+const destinationRoute = require("./routes/destinationRoutes")
 
 connectDB();
 
 app.use(express.json());
 
-app.use("/api/user",userRoute);
+app.use("/api/user",userRoutes);
+app.use("/api/destination",destinationRoute);
 
 const port = 3000;
 app.listen(port,()=>{
