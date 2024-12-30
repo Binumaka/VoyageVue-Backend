ac747bfe8752b0910a9fd675a98d4056d372363b
Binu Maka <binumaka9@gmail.com>
2024-12-30 21:12:25 +0545

    adding business logic in app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const userRoutes= require("./routes/userRoutes");
 const destinationRoute = require("./routes/destinationRoutes");
 const AccommodationRoute = require("./routes/accommodationRoutes");
 const bookingRoute = require("./routes/bookingRoutes");
+const authRoute = require("./routes/authRoutes");
 
 connectDB();
 
@@ -14,6 +15,7 @@ app.use("/api/user",userRoutes);
 app.use("/api/destination",destinationRoute);
 app.use("/api/accommodation",AccommodationRoute);
 app.use("/api/bookings",bookingRoute);
+app.use("/api/auth",authRoute);
 
 const port = 3000;
 app.listen(port,()=>{
