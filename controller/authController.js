567734e9ec96e71d4a2e73b4149040644252dc24
Binu Maka <binumaka9@gmail.com>
2025-01-02 19:29:58 +0545

    auth controller

--- controller/authController.js
+++ controller/authController.js
@@ -1,29 +1,29 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const SECRET_KEY = process.env.SECRET_KEY;
-const Credential = require("../model/Credential");
+const Credential = require("../model/userModel");
 
 const test = (req, res) => {
   res.json("test is working");
 };
 
 const registerUser = async (req, res) => {
-  const { username, password, role } = req.body;
+  const { email, password, role } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
-  const cred = new Credential({ username, password: hashedPassword, role });
+  const cred = new Credential({ email, password: hashedPassword, role });
   cred.save();
   res.status(201).send(cred);
 };
 
 const loginUser = async (req, res) => {
-  const { username, password } = req.body;
-  const cred = await Credential.findOne({ username });
+  const { email, password } = req.body;
+  const cred = await Credential.findOne({ email });
   if (!cred || !(await bcrypt.compare(password, cred.password))) {
     return res.status(403).send("Invalid username or password");
   }
 
   const token = jwt.sign(
-    { username: cred.username, role: cred.role },
+    { email: cred.email, role: cred.role },
     SECRET_KEY,
     { expiresIn: "1h" }
   );
