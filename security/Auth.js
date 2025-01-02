9a6cd822d2eb99a4ca7546ad7d09f81eeeebcb91
Binu Maka <binumaka9@gmail.com>
2025-01-02 19:33:11 +0545

    Added auth.js

--- security/Auth.js
+++ security/Auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../model/user');
 
 const authenticateToken = async (req, res, next) => {
     const token = req.header('Authorization')?.split(' ')[1];
