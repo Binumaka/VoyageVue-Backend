d9782b51acddb3a8b9a402bb51f47f438deb0f48
Binu Maka <binumaka9@gmail.com>
2025-01-03 23:01:24 +0545

    changes in bookingModel

--- model/bookingModel.js
+++ model/bookingModel.js
@@ -1,36 +1,39 @@
 const mongoose = require("mongoose");
 
-const bookingSchema = new mongoose.Schema({
+const bookingSchema = new mongoose.Schema(
+  {
     userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "users",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
     },
     destinationId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "destination",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "destination",
+      required: true,
     },
     accommodationId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "accommodation",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "accommodation",
+      required: true,
     },
     checkInDate: {
-        type: String,
-        required: true,
+      type: Date,
+      required: true,
     },
     checkOutDate: {
-        type: String,
-        required: true,
+      type: Date,
+      required: true,
     },
     totalPrice: {
-        type: Number,
-        required: true,
-    }
-}, {
+      type: Number,
+      required: true,
+    },
+  },
+  {
     timestamps: true, 
-});
+  }
+);
 
 const Booking = mongoose.model("booking", bookingSchema);
 module.exports = Booking;
